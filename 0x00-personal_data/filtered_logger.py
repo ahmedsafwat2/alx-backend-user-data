9497ac6be0a11f3cc3d50d4c5a7f8aa16696e37c
Your Name <you@example.com>
2024-09-07 17:56:54 +0000

    filter

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for personal data project
+"""
+
+
+import logging
+import os
+import mysql.connector
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Returns regex obfuscated log messages """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
