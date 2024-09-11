656432cf9d125fcab74e2538412d023ff13e815e
Your Name <you@example.com>
2024-09-11 12:18:06 +0000

    Authorization

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+
+from flask import request
+from typing import List, TypeVar
+from api.v1.views import User
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    a class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns False - path and excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns None - request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None - request
+        """
+        return None
