f0acdb8400c9f8917932a73141d03adc5ef4c43d
Your Name <you@example.com>
2024-09-11 17:35:50 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,14 @@ class Auth:
         """
         returns False - path and excluded_paths
         """
-        return False
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            check += '/'
+        if path in excluded_paths or check in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
