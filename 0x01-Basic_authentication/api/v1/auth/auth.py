cab8f459d70aef432562df62e57c2d4ae5540eea
Your Name <you@example.com>
2024-09-12 21:49:30 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.views import User
 User = TypeVar('User')
+import fnmatch
 
 
 class Auth:
@@ -17,13 +18,11 @@ class Auth:
         """
         returns False - path and excluded_paths
         """
-        check = path
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path[-1] != '/':
-            check += '/'
-        if path in excluded_paths or check in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
