12d94e82c15e71dc751c9bd8902e289ec158c8bf
Your Name <you@example.com>
2024-09-12 15:48:03 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth module
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic ")):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """base64decode
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            dc_bytes = base64.b64decode(base64_authorization_header)
+            dc_str = dc_bytes.decode('utf-8')
+            return dc_str
+        except(base64.binascii.Error, UnicodeDecodeError):
+            return None
