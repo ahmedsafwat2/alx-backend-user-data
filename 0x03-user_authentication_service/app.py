844c1a5370319c8955302546db2ca11c0c18d7b6
Your Name <you@example.com>
2024-09-18 10:29:53 +0000

    users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,6 +18,7 @@ def home() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def users() -> str:
     """users post route
@@ -29,6 +30,7 @@ def users() -> str:
         return jsonify({"email": f"{email}", "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
