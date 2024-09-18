e64700b0976f700da483b91505a69d3ee1d8a7a6
Your Name <you@example.com>
2024-09-18 10:29:00 +0000

    users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 """app.url_map.strict_slashes = False
 """
 
@@ -16,6 +18,17 @@ def home() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """users post route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
