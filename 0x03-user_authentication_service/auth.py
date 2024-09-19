0a59d1b9ab7f3fc858e50b3e431fe93dee983dc3
Your Name <you@example.com>
2024-09-19 18:30:24 +0000

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,13 +85,14 @@ class Auth:
         except Exception:
             raise ValueError
 
-    def update_password(self, reset_token: str, password: str):
+    def update_password(self, reset_token: str, password: str) -> None:
         """update password
         """
         try:
             user = db.find_user_by(reset_token=reset_token)
             db.update_user(user.id, hashed_password=_hash_password(password),
                            reset_token=None)
+            return None
         except Exception:
             raise ValueError
 
